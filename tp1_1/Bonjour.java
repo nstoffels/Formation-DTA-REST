317b2cd8d40cfe25e567a902b32c00b20074daac
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-06-24 12:28:14 +0200

    démarrage de la formation JAVA SE 7, premiers tp fait.

---
+++ tp1_1/Bonjour.java
@@ -0,0 +1,16 @@
+//premier programme java en java 1.8, première et troisième partie du tp1
+
+package tp1_1;
+public class Bonjour {
+	public static void main(String args[]) { 
+		System.out.println("Bonjour tout le monde"); 
+	} 
+}
+/**  spéicification du chemin pour jdk **/
+/*SET PATH=%PATH%;"C:\Program Files\Java\jdk1.8.0_45\bin"
+SET CLASSPATH=.;"C:\Program Files\Java\jdk1.8.0_45\jre\lib"*/
+/*
+On fait javac Bonjour.java
+On fait ensuite : java Bonjour
+*/
+
---
+++ tp1_2/BonjourPersonnalise.java
@@ -0,0 +1,8 @@
+//seconde partie du tp1
+package tp1_2;
+public class BonjourPersonnalise {
+	public static void main(String[] args) { //pas d'espace entre le string et les crochets !
+		System.out.println("Bonjour " + args[0]); 
+	} 
+}
+//on ne met que des espaces pour préciser les paramètres!
